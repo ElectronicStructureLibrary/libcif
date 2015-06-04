0806e19e99db4f12ba0c293c88eecfe56de1d551
James Spencer <j.spencer@imperial.ac.uk>
2015-06-04 16:04:33 +0200

    Mark vector and matrix functions to be inlined.

--- src/vector3.h
+++ src/vector3.h
@@ -8,39 +8,39 @@ typedef struct Vector3 {
     double x[3];
 } Vector3;
 
-void v3_new(Vector3 *const v, double x, double y, double z) {
+inline void v3_new(Vector3 *const v, double x, double y, double z) {
     v->x[0] = x;
     v->x[1] = y;
     v->x[2] = z;
 }
 
-void v3_from_array(Vector3 *const v, double *x) {
+inline void v3_from_array(Vector3 *const v, double *x) {
     for (int i=0; i<3; i++) v->x[i] = x[i];
 }
 
-Vector3 v3_sum(const Vector3 *const v1, const Vector3 *const v2) {
+inline Vector3 v3_sum(const Vector3 *const v1, const Vector3 *const v2) {
     Vector3 sum;
     for (int i=0; i<3; i++) sum.x[i] = v1->x[i] + v2->x[i];
     return sum;
 }
 
-Vector3 v3_diff(const Vector3 *const v1, const Vector3 *const v2) {
+inline Vector3 v3_diff(const Vector3 *const v1, const Vector3 *const v2) {
     Vector3 diff;
     for (int i=0; i<3; i++) diff.x[i] = v1->x[i] - v2->x[i];
     return diff;
 }
 
-double v3_dot(const Vector3 *const v) {
+inline double v3_dot(const Vector3 *const v) {
     double dot = 0.0;
     for (int i=0; i<3; i++) dot += pow(v->x[i],2);
     return dot;
 }
 
-double v3_len(const Vector3 *const v) {
+inline double v3_len(const Vector3 *const v) {
     return sqrt(v3_dot(v));
 }
 
-bool v3_eq(const Vector3 *const v1, const Vector3 *const v2, double tol) {
+inline bool v3_eq(const Vector3 *const v1, const Vector3 *const v2, double tol) {
     bool eq = true; 
     for (int i=0; i<3; i++) {
         eq = abs(v1->x[i] - v2->x[i]) < tol;
